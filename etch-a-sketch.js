be52872d5fd47cda087cc689e4cb64a3caf16024
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2025-12-25 19:02:48 +0200

    Move square generation logic into function
    Change inner loop from i to j to avoid
    confusion.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,28 +1,30 @@
 const container = document.querySelector('#container');
-let squaresPerRow = 16;
-let squaresPerColumn = squaresPerRow;
-
 // get the width of container
 const containerWidth = container.offsetWidth;
 
-// find square width and height
-let squareWidth = containerWidth / squaresPerRow;
+drawSquareGrid(16);
 
-for (let i = 0; i < squaresPerRow; i++) {
-  // create a div with class row
-  const row = document.createElement('div');
-  row.classList.add('row');
+function drawSquareGrid(squaresPerRow) {
+  const squaresPerColumn = squaresPerRow;
+  // find square width and height
+  let squareWidth = containerWidth / squaresPerRow;
 
-  for (let i = 0; i < squaresPerColumn; i++) {
-    // create a div with class square
-    const square = document.createElement('div');
-    square.classList.add('square');
+  for (let i = 0; i < squaresPerRow; i++) {
+    // create a div with class row
+    const row = document.createElement('div');
+    row.classList.add('row');
 
-    // set the width and height of square
-    square.style.width = `${squareWidth}px`;
-    square.style.height = `${squareWidth}px`;
+    for (let j = 0; j < squaresPerColumn; j++) {
+      // create a div with class square
+      const square = document.createElement('div');
+      square.classList.add('square');
 
-    row.appendChild(square);
+      // set the width and height of square
+      square.style.width = `${squareWidth}px`;
+      square.style.height = `${squareWidth}px`;
+
+      row.appendChild(square);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
 }
